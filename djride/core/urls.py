be01ca9b126671aadb506e6c8833b0796671d7ba
plungerman <plungerman@gmail.com>
2018-06-27 11:41:47 -0500

    added authentication URLs

--- djride/core/urls.py
+++ djride/core/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
+
+from django.contrib.auth import views as auth_views
 
 from django.contrib import admin
 
@@ -10,6 +13,45 @@ handler500 = 'djtools.views.errors.server_error'
 
 
 urlpatterns = [
+    # auth
+    url(
+        r'^accounts/login/$', auth_views.login,
+        {'template_name': 'accounts/login.html'},
+        name='auth_login'
+    ),
+    url(
+        r'^accounts/logout/$', auth_views.logout,
+        {'next_page': reverse_lazy('auth_loggedout')},
+        name='auth_logout'
+    ),
+    url(
+        r'^accounts/loggedout/$', loggedout,
+        {'template_name': 'accounts/logged_out.html'},
+        name='auth_loggedout'
+    ),
+    url(
+        r'^accounts/profile/sender/manager/$', sender_manager,
+        name='sender_manager'
+    ),
+    url(
+        r'^accounts/profile/sender/manager/(?P<sid>\w+)/(?P<action>[-\w]+)/$',
+        sender_manager, name='sender_update'
+    ),
+    url(
+        r'^accounts/profile/$', user_profile,
+        name='user_profile'
+    ),
+    url(
+        r'^accounts/$',
+        RedirectView.as_view(url=reverse_lazy('auth_login'))
+    ),
+    url(
+        r'^denied/$',
+        TemplateView.as_view(
+            template_name='denied.html'
+        ), name='access_denied'
+    ),
+    # django admin
     url(
         r'^admin/', include(admin.site.urls)
     ),
